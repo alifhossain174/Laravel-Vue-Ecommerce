50f6827eda738c9aa9ddce42cbcac38b91caff8f
Fahim <alifhossain174@gmail.com>
2020-12-31 02:24:13 +0600

    Updating Project

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -492,6 +492,7 @@ class ComposerStaticInitf59485e9f3e009e9a5d72df65ad29a28
         'App\\Http\\Middleware\\TrustHosts' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustHosts.php',
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
+        'App\\Model\\Category' => __DIR__ . '/../..' . '/app/Model/Category.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
         'App\\Providers\\BroadcastServiceProvider' => __DIR__ . '/../..' . '/app/Providers/BroadcastServiceProvider.php',
